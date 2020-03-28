5c1c3ad5e70b7c29e7800454e1e0894fd43eb9f4
Thomas Geraghty <thomascgeraghty@outlook.com>
2020-03-28 16:30:59 +0000

    Heatmap function

--- API/Server.js
+++ API/Server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const config = require('../config');
+const config = require('./config');
 const crimesController = require('./controllers/crimesController')
 app.use(cors());
 app.options('*', cors());
 app.set('json spaces', 4)
 
-crimesController.intialize.then(() => { routes() });
+crimesController.intialize().then(() => { 
+  routes() 
+});
 
 function routes() {
-  app.get('/api/crimes/', (req, res) => {
+  app.get('/api/crimes', (req, res) => {
     const index = req.query.index;
     const limit = req.query.limit
 
@@ -19,6 +21,7 @@ function routes() {
     });
   });
 
+  /*
   app.get('/api/crimes/:id', (req, res) => {
     const id = req.params.id;
 
@@ -27,10 +30,22 @@ function routes() {
     });
   });
 
+  */
+
+  app.get('/api/crimes/search', (req, res) => {
+    const index = req.query.index;
+    const limit = req.query.limit
+    const searchString = req.query.search
+
+    crimesController.getCrimes(index, limit).then(result => {
+      res.json(result);
+    });
+  });
+
   app.get('/api/crimes/nearby', (req, res) => {
     const location = {
-      lat = req.query.lat,
-      lng = req.query.lng
+      lat: req.query.lat,
+      lng: req.query.lng
     }
     const distance = req.query.dist;
 
@@ -40,6 +55,15 @@ function routes() {
     })
   });
 
+  app.get('/api/crimes/heatmap', (req, res) => {
+    let boundingBox = {
+      NE: req.query.ne.split(','),
+      SW: req.query.sw.split(',')
+    }
+    
+    crimesController.getHeatmap(boundingBox);
+  });
+
   app.get('/api/crimes/regions/', (req, res) => {
     res.json(crimesController.getRegions);
   });
@@ -47,6 +71,8 @@ function routes() {
   app.get('/api/crimes/types/', (req, res) => {
     res.json(crimesController.getCrimeTypes);
   });
+
+
 }
 
 var server = app.listen(config.server.port, config.server.host, () => {
--- API/controllers/crimesController.js
+++ API/controllers/crimesController.js
@@ -4,7 +4,7 @@ var crimeTypes = [];
 var regions = [];
 
 function intialize() {
-    return new Promise(resolve, () => {
+    return new Promise((resolve) => {
         mongodb.intialize()
         .then(() => {
             var promises = [
@@ -71,15 +71,56 @@ function getCrimesByRegion(region, index, limit) {
     return mongodb.getRecords('PoliceData', { query: { CrimeType: region }, index: index, limit: limit });
 }
 
+function getSearchResults(query) {
+    return mongodb.getRecords('PoliceData', { query: { $text: { $search: query } } });
+}
+
+function getHeatmap(boundingBox, divisions) {
+    divisions = 3;
+
+    //MAKE REQUEST TO DB FOR INTERSECTS BETWEEN THESE
+    const crimes = [];
+    const crimesRegions = [];
+
+    console.log(boundingBox)
+    let length = Math.abs(boundingBox.NE[0] - boundingBox.SW[0]) / divisions;
+
+    for(lat = boundingBox.NE[0]; lat > boundingBox.SW[0]; lat -= length) {
+        for(lon = boundingBox.NE[1]; lon > boundingBox.SW[1]; lon -= length) {
+            console.log(lon)
+            let b = {
+                boundingBox: {
+                    NE: [lat + length/2, lon - length/2],
+                    SW: [lat - length/2, lon + length/2]
+                },
+                intensity: 0
+            };
+
+            for(crime in crimes) {
+                if((crime.lat > lat - length/2 && crime.lat < lat + length/2)
+                && (crime.lon > lon - length/2 && crime.lon < lon + length/2)) {
+                    b.intensity++;
+                }
+            }
+
+            crimesRegions.push(b);
+        }
+    }
+
+
+    return crimesRegions;
+}
 
 module.exports = {
     intialize: intialize,
     getCrimeTypes: getCrimeTypes,
     getRegions: getRegions,
-    
+
     getCrime: getCrime,
     getCrimes: getCrimes,
     getCrimesNearby: getCrimesNearby,
     getCrimesByType: getCrimesByType,
-    getCrimesByRegion: getCrimesByRegion
+    getCrimesByRegion: getCrimesByRegion,
+    getSearchResults: getSearchResults,
+    getHeatmap: getHeatmap
 }
