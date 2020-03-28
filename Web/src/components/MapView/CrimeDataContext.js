c9b7ee24bb60b7195683f3893d5645939dc20aaf
Thomas Geraghty <thomascgeraghty@outlook.com>
2020-03-28 19:31:21 +0000

    Updated to use CrimeDataContext

---
+++ Web/src/components/MapView/CrimeDataContext.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { MainContext } from './MainContext'
+import { getNearbyCrimes } from '../../client/API';
+
+export const CrimeDataContext = React.createContext();
+
+const initialState = {
+  nearby: []
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'RESET':
+      let reset = {
+        nearby: state.nearby
+      };
+      return reset;
+    case 'SET_NEARBY':
+      return { ...state, nearby: action.payload };
+  }
+};
+
+export default ({ children }) => {
+  const [ cState, cDispatch ] = React.useReducer(reducer, initialState)
+  const { mState } = React.useContext(MainContext);
+
+  // Get Nearby Crime Data
+  useEffect(() => {
+    if (mState.viewport) {
+      getNearbyCrimes(mState.viewport.center, Math.pow((20 - mState.viewport.zoom), 2.5) * 30)
+        .then(result => {
+          cDispatch({ type: 'SET_NEARBY', payload: result })
+        });
+    }
+  }, [mState.viewport])
+
+  const CrimeData = { cState, cDispatch }
+
+  return <CrimeDataContext.Provider value={CrimeData}>{children}</CrimeDataContext.Provider>
+}
