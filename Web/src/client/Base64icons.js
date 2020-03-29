c8b02618d0ce997313fee00ba9fb1639f5b002fe
Ollie Nye <ollie@nyemail.co.uk>
2020-03-29 23:20:48 +0100

    Tweak circle generator method

--- Web/src/client/Base64icons.js
+++ Web/src/client/Base64icons.js
@@ -6,7 +6,7 @@ export const icons = {
 
 export function svgIconHandler(value, distribution_point) {
 
-    value = value * 25
+    value = distribution_point
 
     var sizeValue = Math.min(Math.max(value/2, 25), 200)
     // var colorValue = 100 - Math.min(Math.max(value/2, 0), 100);
@@ -19,14 +19,8 @@ export function svgIconHandler(value, distribution_point) {
 
     function perc2color(perc) {
         var r, g, b = 0;
-        if (perc < 50) {
-            r = 255;
-            g = Math.round(5.1 * perc);
-        }
-        else {
-            g = 255;
-            r = Math.round(510 - 5.1 * perc);
-        }
+        r = 255;
+        g = Math.round((perc / 100) * 255);
         var h = r * 0x10000 + g * 0x100 + b * 0x1;
         return '#' + ('000000' + h.toString(16)).slice(-6);
     }
