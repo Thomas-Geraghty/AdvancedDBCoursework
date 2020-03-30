86278a43fc9b325d1dde000bff62b4bd8769ba17
Thomas Geraghty <thomascgeraghty@outlook.com>
2020-03-30 19:28:31 +0100

    Test charts

--- Web/src/components/ChartView/ChartView.js
+++ Web/src/components/ChartView/ChartView.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import './ChartView.scss'
+import { Doughnut } from 'react-chartjs-2';
 
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
 
 export default function ChartView() {
 
     // Render
     return (
-        <h3>Charts!</h3>
+        <div className="ChartView">
+            <h3>Charts</h3>
+            <div className="charts-row">
+                <div>
+                    <h4>Test chart</h4>
+                    <Doughnut data={data}/>
+                </div>
+                <div>
+                    <h4>Test chart</h4>
+                    <Doughnut data={data} />
+                </div>
+            </div>
+        </div>
     )
 }
